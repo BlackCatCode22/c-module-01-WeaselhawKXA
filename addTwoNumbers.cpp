35775e1cad57f61c3099c64f88d28b04ede11dde
WeaselhawKXA <puckybyrd99@gmail.com>
2025-08-13 12:26:55 -0700

    Add files via upload

---
+++ addTwoNumbers.cpp
@@ -0,0 +1,24 @@
+// Created by BE129 on 8/13/2025.
+// addTwoNumbers.cpp
+// 0836901
+// Create variables needed for this program
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int num1 = 0;
+    int num2 = 0;
+    int sumOFNums = 0;
+    cout << "Enter a value for num1:";
+    cin >> num1;
+    cout << "Enter a value for num2:";
+    cin >> num2;
+
+    sumOFNums = num1 + num2;
+
+    cout <<  "\n" "The sum of " << num1 << " and " << num2 << " is " << sumOFNums;
+
+    return 0;
+
+}
